6a0edf90dad456f37f7f7a5927869e4d16b3c155
SadiqurApon <sadiqurrahmanapon34@gmail.com>
2025-08-29 22:33:59 +0600

    added a hideBlock js folder

---
+++ script/hideBlock.js
@@ -0,0 +1,12 @@
+document.getElementById('add-money-box')
+    .addEventListener('click', function () {
+        document.getElementById("cash-out-section").style.display = 'none';
+        document.getElementById("add-money-section").style.display = 'block';
+
+    })
+
+document.getElementById('cashout-box')
+    .addEventListener('click', function () {
+        document.getElementById('add-money-section').style.display = 'none';
+        document.getElementById('cash-out-section').style.display = 'block';
+    })
