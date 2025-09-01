33cc743948a7c5ccd5852260db3c356ba4c4b3a4
SadiqurApon <sadiqurrahmanapon34@gmail.com>
2025-09-01 20:06:05 +0600

    practice

--- demo.js
+++ demo.js
@@ -39,3 +39,58 @@ function getInnerTextById(id){
 function getInnerTextByIdAndValue(id,value){
     document.getElementById(id).innerText = value;
 }
+
+// demoAcount er code ekhane rakhlam 
+document.getElementById("add-money")
+    .addEventListener("click", function (event) {
+        event.preventDefault();
+        //console.log("hello ");
+        // getInputValueById(); //utilites file tare eikhane call kora jay
+
+        // const value = getInputValueById();
+        // console.log(value);                  //utilites er hello bro ta ke variable er maddome return korsi
+        
+        // const account = getInputValueById("acount-number");
+        
+        
+        const amount = getInputValueById("add-amount");
+        const pin = getInputValueById("pin");
+        const acount = document.getElementById("acount-number").value;
+        
+        // console.log(account,amount, pin);
+        
+        const mainBalance = getInnerTextById("main-balance");
+        
+        // console.log(mainBalance);
+
+
+        if (acount.length === 8) {
+            if (pin === 1234) {
+                const sum = mainBalance + amount;
+                // document.getElementById("main-balance").innerText = sum;
+                setInnerTextByIdandValue("main-balance" , sum);
+            } else {
+                console.log("Enter valid PIN");
+            }
+        } else {
+            console.log("Enter valid Number");
+        }
+    })
+
+// utilites er conde gola o ekhane ralhlam
+
+function getInputValueById(id){
+    const value = document.getElementById(id).value;
+    const convertedValue = parseFloat(value);
+    return convertedValue;
+}
+
+function getInnerTextById(id){
+    const value = document.getElementById(id).innerText;
+    const convertedValue = parseFloat(value);
+    return convertedValue;
+}
+
+function setInnerTextByIdandValue(id, value){
+    document.getElementById(id).innerText = value;
+}
--- script/demoAddMoney.js
+++ script/demoAddMoney.js
@@ -1,35 +1,26 @@
 document.getElementById("add-money")
     .addEventListener("click", function (event) {
         event.preventDefault();
-        //console.log("hello ");
-        // getInputValueById(); //utilites file tare eikhane call kora jay
-
-        // const value = getInputValueById();
-        // console.log(value);                  //utilites er hello bro ta ke variable er maddome return korsi
-        
-        // const account = getInputValueById("acount-number");
-        
-        
         const amount = getInputValueById("add-amount");
         const pin = getInputValueById("pin");
+        // const acount = getInputValueById("acount-number");
         const acount = document.getElementById("acount-number").value;
-        
-        // console.log(account,amount, pin);
-        
+        // console.log(amount, pin, acount);
         const mainBalance = getInnerTextById("main-balance");
-        
         // console.log(mainBalance);
-
-
         if (acount.length === 8) {
             if (pin === 1234) {
                 const sum = mainBalance + amount;
+                // console.log(sum);
                 // document.getElementById("main-balance").innerText = sum;
-                setInnerTextByIdandValue("main-balance" , sum);
+                setInnerTextByIdandValue("main-balance", sum);
+                
             } else {
                 console.log("Enter valid PIN");
             }
-        } else {
+        }
+        else {
             console.log("Enter valid Number");
         }
+
     })
