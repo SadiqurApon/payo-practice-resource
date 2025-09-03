ca58376d827befc4fc7f0bfd1431e1c7c04f4968
SadiqurApon <sadiqurrahmanapon34@gmail.com>
2025-09-03 10:13:17 +0600

    Transection Box blocked

--- utility/toggle.js
+++ utility/toggle.js
@@ -6,11 +6,20 @@ document.getElementById("add-money-box")
         // document.getElementById("add-money-section").style.display = "block";
         handleToggle("cash-out-section", "none");
         handleToggle("add-money-section", "block");
+        handleToggle("transaction-section", "none");
     })
 
 document.getElementById("cashout-box")
     .addEventListener("click", function () {
         handleToggle("cash-out-section", "block");
+        handleToggle("add-money-section", "none");
+        handleToggle("transaction-section", "none");
+    })
+// Transection History block
+document.getElementById("transection-box")
+    .addEventListener("click", function () {
+        handleToggle("transaction-section", "block");
+        handleToggle("cash-out-section", "none");
         handleToggle("add-money-section", "none");
     })
 
